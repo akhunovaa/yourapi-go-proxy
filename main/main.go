de456b0578189e20571c1e8cda1fb3b26a73a72e
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-17 21:26:40 +0300

    [NF] project init

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -10,15 +11,35 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		keys, ok := r.URL.Query()["url"]
-		key := keys[0]
+		url := keys[0]
 
 		if !ok || len(keys[0]) < 1 {
 			log.Println("Url Param 'url' is missing")
-			return
 		}
-		log.Println("Url Param 'key' is: " + string(key))
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path+" KEY: "+key))
+		log.Println("Url Param 'key' is: " + string(url))
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		requestedBody := processServer(url)
+		fmt.Fprintf(w, html.EscapeString(requestedBody))
 	})
 	log.Println("Listening on localhost:7733")
 	log.Fatal(http.ListenAndServe(":7733", nil))
+}
+
+func processServer(s string) string {
+	fmt.Println(s, " : started processing")
+	resp, err := http.Get(s)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	responseBody := string(body)
+	log.Println(string(responseBody))
+	return responseBody
 }
