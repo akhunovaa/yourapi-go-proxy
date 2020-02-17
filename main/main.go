2595475d5772fc87351c592d04a89ceb3c0f40d3
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-17 20:37:26 +0300

    [NF] project init

--- main/main.go
+++ main/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
 )
 
 func main() {
+	verbose := flag.Bool("v", true, "should every proxy request be logged to stdout")
+	addr := flag.String("addr", ":7733", "proxy listen address")
+	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":7733", proxy))
+	proxy.Verbose = *verbose
+	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
